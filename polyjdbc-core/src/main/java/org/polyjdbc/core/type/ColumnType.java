2ecd1617160e6331039974a380a639fef6c29546
Adam Dubiel <adam.dubiel@allegro.pl>
2013-11-21 12:00:30 +0100

    text column type fix - longvarchar instead of clob

--- polyjdbc-core/src/main/java/org/polyjdbc/core/type/ColumnType.java
+++ polyjdbc-core/src/main/java/org/polyjdbc/core/type/ColumnType.java
@@ -26,7 +26,7 @@ import org.polyjdbc.core.exception.UnknownColumnTypeException;
 public enum ColumnType {
 
     STRING(String.class, Types.VARCHAR),
-    TEXT(Text.class, Types.CLOB),
+    TEXT(Text.class, Types.LONGNVARCHAR),
     INT(Integer.class, Types.INTEGER),
     LONG(Long.class, Types.BIGINT),
     CHAR(Character.class, Types.CHAR),
