8edfff2180821c2e2a58287eb1e4b288707cbb5d
Bartosz Walacik <bartosz.walacik@allegro.pl>
2015-06-24 22:18:35 +0200

    oracle rownum fixes

--- src/main/java/org/polyjdbc/core/dialect/OracleDialectQueries.java
+++ src/main/java/org/polyjdbc/core/dialect/OracleDialectQueries.java
@@ -5,7 +5,7 @@ import org.polyjdbc.core.query.Query;
 public class OracleDialectQueries implements DialectQueries {
     @Override
     public void limit(Query query, Limit limit) {
-        query.wrap("SELECT a.*, rownum r__ FROM (", ")a WHERE r__ BETWEEN"
+        query.wrap("SELECT a.*, rownum r__ FROM (", ")a WHERE rownum BETWEEN "
                 + limit.getOffset() + " AND " + (limit.getOffset() + limit.getLimit()));
     }
 }
